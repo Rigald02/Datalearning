24e3f4c11fbbd7864c4ba82b5b75d90706888e6c
Rigald02 <h.debelsunce@hotmail.fr>
2026-02-16 19:14:29 +0100

    if, else if, else

--- python/premiertry.py
+++ python/premiertry.py
@@ -2,5 +2,9 @@ class my_class(object):
     print("je serais data analyst!")
     print("quel est ton nom?")
     char = input()
-    print("Enchanté "+char+ "Comment vas-tu aujourd'hui?")
+    print("Enchanté "+char+ ", comment vas-tu aujourd'hui?")
+    mood = input()
+    if mood == "bien": print("je suis heureux de l'apprendre")
+    elif mood == "bof": print("Ca pourrait aller mieux, c'est ça?")
+    else: print("je ne comprend pas tout malheureusement")
     pass
