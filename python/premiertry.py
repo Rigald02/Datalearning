415aee35402336ba3f0e641012abcce064c9b675
Rigald02 <h.debelsunce@hotmail.fr>
2026-02-16 19:29:04 +0100

    tests entre else if et elif

--- python/premiertry.py
+++ python/premiertry.py
@@ -5,6 +5,14 @@ class my_class(object):
     print("Enchanté "+char+ ", comment vas-tu aujourd'hui?")
     mood = input()
     if mood == "bien": print("je suis heureux de l'apprendre")
+    elif mood == "mal": print("bah, ca ira mieux demain!")
     elif mood == "bof": print("Ca pourrait aller mieux, c'est ça?")
-    else: print("je ne comprend pas tout malheureusement")
+    elif mood := "ca peut aller": print ("prend un coup de coca ca reboost")
+    elif mood := "la cata": print("va dormir ca ira mieux demain")
+    else: print("je ne comprends pas tout malheureusement")
     pass
+#else if n'existe pas:
+#if est une option, elif les autres option, else est toute les autres option.
+#basiquement, if et elif sont des cas de boolean true, else est un boolean false
+
+# les symboles := et == sont la meme chose(?)
